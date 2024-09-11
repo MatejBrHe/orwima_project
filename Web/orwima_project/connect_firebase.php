7da72ab785c7abfd297a2d3f1441885633daaf3e
Matej <matejbh@gmail.com>
2024-09-11 10:20:06 +0200

    Removed a file that must not be in public repo and commented the explanation in code

--- Web/orwima_project/connect_firebase.php
+++ Web/orwima_project/connect_firebase.php
@@ -4,8 +4,8 @@
 	use Kreait\Firebase\Factory;
 
 	$firebase = (new Factory)
-		->withServiceAccount('orwima-project-mbh-firebase-adminsdk-dsirb-565892b115.json')
-		->withDatabaseUri('https://orwima-project-mbh-default-rtdb.europe-west1.firebasedatabase.app/');
+		->withServiceAccount('orwima-project-mbh-firebase-adminsdk-dsirb-565892b115.json')  				//This json file has been removed from the repository because Google disables access to a firebase databes if it is uploaded to public repository.
+		->withDatabaseUri('https://orwima-project-mbh-default-rtdb.europe-west1.firebasedatabase.app/');	//To access the database you need to create your own json file inside the firebase web interface and paste the name here.
 
 	$database = $firebase->createDatabase();
 
